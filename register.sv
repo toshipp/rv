22749c76b6bdccf4a7a0bb7c14b7d9a30143b337
toshipp <toshiq2@gmail.com>
2020-05-30 16:05:44 +0900

    add register

---
+++ register.sv
@@ -0,0 +1,11 @@
+module register (input logic clk,
+                 input logic         reset,
+                 input logic [31:0]  in,
+                 input logic         write_enable,
+                 output logic [31:0] out);
+   always_ff @(posedge clk, posedge reset)
+     if(reset)
+       out <= 0;
+     else if(write_enable)
+       out <= in;
+endmodule
