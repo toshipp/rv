a727e49763519b2474cdb4cfa0d2265fd9339864
toshipp <toshiq2@gmail.com>
2020-07-23 14:50:54 +0900

    fix comparer to handle GE properly

--- comparer.sv
+++ comparer.sv
@@ -13,7 +13,7 @@ module comparer
        `CMP_LT2: out = $signed(in1) < $signed(in2);
        `CMP_LTU2: out = in1 < in2;
        `CMP_LT: out = $signed(in1) < $signed(in2);
-       `CMP_GE: out = $signed(in2) >= $signed(in2);
+       `CMP_GE: out = $signed(in1) >= $signed(in2);
        `CMP_LTU: out = in1 < in2;
        `CMP_GEU: out = in1 >= in2;
      endcase
