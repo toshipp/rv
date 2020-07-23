2113ae041b40de7e6acfe40668db5c8e0f90b057
toshipp <toshiq2@gmail.com>
2020-07-23 15:10:21 +0900

    fix load instruction

--- load_memory_decoder.sv
+++ load_memory_decoder.sv
@@ -8,15 +8,15 @@ module load_memory_decoder(input logic [2:0] type_,
      case(type_)
        `LOAD_B:
          case(offset)
-           0: out = {24'b0, in[7:0]};
-           1: out = {24'b0, in[15:8]};
-           2: out = {24'b0, in[23:16]};
-           3: out = {24'b0, in[31:24]};
+           0: out = {{24{in[7]}}, in[7:0]};
+           1: out = {{24{in[15]}}, in[15:8]};
+           2: out = {{24{in[23]}}, in[23:16]};
+           3: out = {{24{in[31]}}, in[31:24]};
          endcase
        `LOAD_H:
          case(offset)
-           0: out = {16'b0, in[15:0]};
-           2: out = {16'b0, in[31:16]};
+           0: out = {{16{in[15]}}, in[15:0]};
+           2: out = {{16{in[31]}}, in[31:16]};
            default: out = 'bx; // trap?
          endcase
        `LOAD_W:
@@ -26,15 +26,15 @@ module load_memory_decoder(input logic [2:0] type_,
          endcase
        `LOAD_BU:
          case(offset)
-           0: out = {{24{in[7]}}, in[7:0]};
-           1: out = {{24{in[15]}}, in[15:8]};
-           2: out = {{24{in[23]}}, in[23:16]};
-           3: out = {{24{in[31]}}, in[31:24]};
+           0: out = {24'b0, in[7:0]};
+           1: out = {24'b0, in[15:8]};
+           2: out = {24'b0, in[23:16]};
+           3: out = {24'b0, in[31:24]};
          endcase
        `LOAD_HU:
          case(offset)
-           0: out = {{16{in[15]}}, in[15:0]};
-           2: out = {{16{in[31]}}, in[31:16]};
+           0: out = {16'b0, in[15:0]};
+           2: out = {16'b0, in[31:16]};
            default: out = 'bx; // trap?
          endcase
        default: out = 'bx;
--- load_memory_decoder_test.sv
+++ load_memory_decoder_test.sv
@@ -12,40 +12,40 @@ module load_memory_decoder_test();
       // LB
       type_ = 3'b000;
       offset = 0;
-      in = 32'hxxxxxxff;
+      in = 32'hxxxxxxbf;
       #1;
-      assert(out === 32'h000000ff) else $fatal;
+      assert(out === 32'hffffffbf) else $fatal;
 
       type_ = 3'b000;
       offset = 1;
-      in = 32'hxxxxffxx;
+      in = 32'hxxxxbfxx;
       #1;
-      assert(out === 32'h000000ff) else $fatal;
+      assert(out === 32'hffffffbf) else $fatal;
 
       type_ = 3'b000;
       offset = 2;
-      in = 32'hxxffxxxx;
+      in = 32'hxxbfxxxx;
       #1;
-      assert(out === 32'h000000ff) else $fatal;
+      assert(out === 32'hffffffbf) else $fatal;
 
       type_ = 3'b000;
       offset = 3;
-      in = 32'hffxxxxxx;
+      in = 32'hbfxxxxxx;
       #1;
-      assert(out === 32'h000000ff) else $fatal;
+      assert(out === 32'hffffffbf) else $fatal;
 
       // LH
       type_ = 3'b001;
       offset = 0;
-      in = 32'hxxxxffff;
+      in = 32'hxxxxbfff;
       #1;
-      assert(out === 32'h0000ffff) else $fatal;
+      assert(out === 32'hffffbfff) else $fatal;
 
       type_ = 3'b001;
       offset = 2;
-      in = 32'hffffxxxx;
+      in = 32'hbfffxxxx;
       #1;
-      assert(out === 32'h0000ffff) else $fatal;
+      assert(out === 32'hffffbfff) else $fatal;
 
       // LW
       type_ = 3'b010;
@@ -57,39 +57,39 @@ module load_memory_decoder_test();
       // LBU
       type_ = 3'b100;
       offset = 0;
-      in = 32'hxxxxxxbf;
+      in = 32'hxxxxxxff;
       #1;
-      assert(out === 32'hffffffbf) else $fatal;
+      assert(out === 32'h000000ff) else $fatal;
 
       type_ = 3'b100;
       offset = 1;
-      in = 32'hxxxxbfxx;
+      in = 32'hxxxxffxx;
       #1;
-      assert(out === 32'hffffffbf) else $fatal;
+      assert(out === 32'h000000ff) else $fatal;
 
       type_ = 3'b100;
       offset = 2;
-      in = 32'hxxbfxxxx;
+      in = 32'hxxffxxxx;
       #1;
-      assert(out === 32'hffffffbf) else $fatal;
+      assert(out === 32'h000000ff) else $fatal;
 
       type_ = 3'b100;
       offset = 3;
-      in = 32'hbfxxxxxx;
+      in = 32'hffxxxxxx;
       #1;
-      assert(out === 32'hffffffbf) else $fatal;
+      assert(out === 32'h000000ff) else $fatal;
 
       // LHU
       type_ = 3'b101;
       offset = 0;
-      in = 32'hxxxxbfff;
+      in = 32'hxxxxffff;
       #1;
-      assert(out === 32'hffffbfff) else $fatal;
+      assert(out === 32'h0000ffff) else $fatal;
 
       type_ = 3'b101;
       offset = 2;
-      in = 32'hbfffxxxx;
+      in = 32'hffffxxxx;
       #1;
-      assert(out === 32'hffffbfff) else $fatal;
+      assert(out === 32'h0000ffff) else $fatal;
    end
 endmodule
